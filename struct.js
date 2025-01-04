4b0163e8a514c9812f1e8427c577f5312f23c7d6
saidhruvatheratipally <saidhruvatheratipally@gmail.com>
2025-01-04 13:31:05 +0530

    starting

---
+++ struct.js
@@ -0,0 +1,18 @@
+let a=[1,2,3,4,5]
+let [b,...c]=a;
+console.log(c)
+// ----------------------
+let k={
+    name:"mani",
+    age:12,
+    address:{
+        city:"hyd",
+        vill:"vvvv"
+    }
+}
+
+console.log(k)
+const {name}=k;
+var {address,vill}=k;
+console.log(name,vill);
+// ------------------------
